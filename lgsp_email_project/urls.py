9d08b0391a9a82adfff7e52b839ef4a1f8d3e0ff
Subith O U <ousubith@gmail.com>
2023-05-14 23:54:08 -0400

    final update

--- lgsp_email_project/urls.py
+++ lgsp_email_project/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('email_app.urls')),
 ] 
+ 
+urlpatterns += staticfiles_urlpatterns()
